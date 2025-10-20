8743261a8d6d3b379f8cd0df4abd6109f465fe87
hinoneko <yanasiryk35@gmail.com>
2025-10-20 17:21:37 +0300

    feat: add dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import views
+from app.user import user_routes
+from app.category import category_routes
+from app.record import record_routes
+from app.views import views_routes
