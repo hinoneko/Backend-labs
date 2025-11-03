28375b80d4efbb13b2a0a6a5c9a69e6ccb96d734
hinoneko <yanasiryk35@gmail.com>
2025-11-03 22:32:30 +0200

    feat: add app startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app.user import user_routes
-from app.category import category_routes
-from app.record import record_routes
-from app.views import views_routes
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_object('app.config.Config')
+
+    db.init_app(app)
+
+    from app.models import User, Category, Record
+
+    migrate.init_app(app, db)
+
+    from app.user.user_routes import user_bp
+    from app.category.category_routes import category_bp
+    from app.record.record_routes import record_bp
+    from app.views.views_routes import views_bp
+
+    app.register_blueprint(views_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(category_bp)
+    app.register_blueprint(record_bp)
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    PROPAGATE_EXCEPTIONS = True
+    FLASK_DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    API_TITLE = "Finance REST API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
