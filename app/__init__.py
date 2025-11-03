195375f86a8586835fe5517e968e8d58077df1c2
hinoneko <yanasiryk35@gmail.com>
2025-11-03 23:24:04 +0200

    feat: migrations on startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,26 @@
+import os
+import time
+
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, init, migrate as fmigrate, upgrade, stamp
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+def is_first_run():
+    return not os.path.exists('migrations')
+
+
+def migrate_db(app):
+    with app.app_context():
+        if is_first_run():
+            init()
+            stamp()
+
+        fmigrate(message="Auto migration")
+
+        upgrade()
 
 
 def create_app():
@@ -16,6 +33,8 @@ def create_app():
     from app.models import User, Category, Record
 
     migrate.init_app(app, db)
+    time.sleep(5)
+    migrate_db(app)
 
     from app.user.user_routes import user_bp
     from app.category.category_routes import category_bp
