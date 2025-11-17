52477440b9242aa39d01bb7fd03c9cad40cb0bb9
hinoneko <yanasiryk35@gmail.com>
2025-11-17 18:18:17 +0200

    feat: add secret key

--- app/config.py
+++ app/config.py
@@ -11,3 +11,4 @@ class Config:
     OPENAPI_URL_PREFIX = "/"
     OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
